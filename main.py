9ff258f56868455be5a8f0d1a93594c210bdbb31
pamarkuldeeep <pamark3087@gmail.com>
2022-11-21 12:35:01 +0530

    add gunicorn

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
 from wtforms import Form, validators
 from flask_gravatar import Gravatar
-from decouple import config
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['SECRET_KEY'] = ('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
@@ -204,29 +204,28 @@ def add_new_post():
     return render_template("make-post.html", form=form,current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>",methods=["GET","POST"])
 @admin_only
 def edit_post(post_id):
-
     post = BlogPost.query.get(post_id)
+    # if post.
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
+        # author_id=current_user.id,#author=current_user,#author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        # post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
 
-    return render_template("make-post.html", form=edit_form,current_user=current_user)
-
+    return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
 
 @app.route("/delete/<int:post_id>")
 @admin_only
